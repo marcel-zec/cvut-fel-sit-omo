daa381c077e2e575f72583e44fe88f00a72d6517
Marcel Žec <zecmarce@fel.cvut.cz>
2019-12-24 12:13:29 +0100

    sposob nastavovanie poradia line workerov

--- src/main/java/cz/cvut/fel/omo/hamrazec/model/LineWorker.java
+++ src/main/java/cz/cvut/fel/omo/hamrazec/model/LineWorker.java
@@ -4,4 +4,18 @@ import main.java.cz.cvut.fel.omo.hamrazec.model.production.Product;
 
 public interface LineWorker {
     void work(Product product);
+
+    /**
+     * Set next line worker in chain of responsibility in production line.
+     * @param nextWorker
+     * @return set worker
+     */
+    LineWorker setNextWorker(LineWorker nextWorker);
+
+    /** //TODO - prelozit a dopisat
+     * Set share in production to line workers. It is value for /prelozit percento opracovania/.
+     * @param shareInProduction
+     * @return itself
+     */
+    LineWorker setShareInProduction(int shareInProduction);
 }
--- src/main/java/cz/cvut/fel/omo/hamrazec/services/builders/BuilderProductA.java
+++ src/main/java/cz/cvut/fel/omo/hamrazec/services/builders/BuilderProductA.java
@@ -1,5 +1,7 @@
 package main.java.cz.cvut.fel.omo.hamrazec.services.builders;
 
+import main.java.cz.cvut.fel.omo.hamrazec.model.LineWorker;
+
 public class BuilderProductA extends LineBuilder {
     //TODO - builderA
 
@@ -13,7 +15,24 @@ public class BuilderProductA extends LineBuilder {
 
     @Override
     public void setOrder() {
-
+        robotList.get(0)
+                .setShareInProduction(0)
+                .setNextWorker(machineList.get(0))
+                .setShareInProduction(0)
+                .setNextWorker(machineList.get(1))
+                .setShareInProduction(0)
+                .setNextWorker(machineList.get(2))
+                .setShareInProduction(0)
+                .setNextWorker(peopleList.get(0))
+                .setShareInProduction(0)
+                .setNextWorker(robotList.get(1))
+                .setShareInProduction(0)
+                .setNextWorker(robotList.get(2))
+                .setShareInProduction(0)
+                .setNextWorker(machineList.get(3))
+                .setShareInProduction(0)
+                .setNextWorker(peopleList.get(1))
+                .setShareInProduction(0)
+                .setNextWorker(controllingRobot);
     }
-
 }
