b4466dfc41fa824e0f81d5738b4094ea17da5051
Sandra Hamráková <hamrasan@fel.cvut.cz>
2019-12-27 16:55:34 +0100

    visitor implements

--- src/main/java/cz/cvut/fel/omo/hamrazec/model/person/Director.java
+++ src/main/java/cz/cvut/fel/omo/hamrazec/model/person/Director.java
@@ -1,7 +1,15 @@
 package cz.cvut.fel.omo.hamrazec.model.person;
 
-public class Director extends Person {
+import cz.cvut.fel.omo.hamrazec.model.Visitor;
+
+public class Director extends Person implements Visitor {
     public Director(String firstName, String lastName, int wage) {
         super(firstName, lastName, wage);
+    }
+
+
+    @Override
+    public void visit(Object o) {
+
     }
 }
--- src/main/java/cz/cvut/fel/omo/hamrazec/model/person/Inspector.java
+++ src/main/java/cz/cvut/fel/omo/hamrazec/model/person/Inspector.java
@@ -1,7 +1,15 @@
 package cz.cvut.fel.omo.hamrazec.model.person;
 
-public class Inspector extends Person {
+import cz.cvut.fel.omo.hamrazec.model.Visitor;
+
+public class Inspector extends Person implements Visitor {
     public Inspector(String firstName, String lastName, int wage) {
         super(firstName, lastName, wage);
+    }
+
+
+    @Override
+    public void visit(Object o) {
+
     }
 }
